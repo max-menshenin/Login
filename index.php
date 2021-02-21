5ce60971d31e329b7022650d627a8c15fe1332cc
postmind <max.menshenin.dev@yandex.ru>
2021-02-21 18:22:56 +0300

    started to convert from cookie to session

--- index.php
+++ index.php
@@ -16,43 +16,43 @@ session_start();
     <?php
     if ($_SESSION['user'] == ''):
     ?>
-<div class="row">
-    <div class="col">
-        <form action="validation-form/check.php" method="post">
-            <h1>Registration form</h1>
-            <input type="text" class="form-control" name="login" id="login"
-                   placeholder="Enter login">
-            <br>
-            <input type="text" class="form-control" name="name" id="name"
-                   placeholder="Enter name">
-
-            <br>
-            <input type="password" class="password" name="pass" id="pass"
-                   placeholder="Enter pass">
-            <br>
-            <button class="btn btn-success" type="submit">Submit</button>
-            <br>
-        </form>
-    </div>
     <div class="row">
         <div class="col">
-            <form action="validation-form/auth.php" method="post">
-                <h1>Auth form</h1>
+            <form action="validation-form/check.php" method="post">
+                <h1>Registration form</h1>
                 <input type="text" class="form-control" name="login" id="login"
                        placeholder="Enter login">
+                <br>
+                <input type="text" class="form-control" name="name" id="name"
+                       placeholder="Enter name">
+
                 <br>
                 <input type="password" class="password" name="pass" id="pass"
                        placeholder="Enter pass">
                 <br>
-                <button class="btn btn-success" type="submit">Auth</button>
+                <button class="btn btn-success" type="submit">Submit</button>
                 <br>
             </form>
         </div>
+        <div class="row">
+            <div class="col">
+                <form action="validation-form/auth.php" method="post">
+                    <h1>Auth form</h1>
+                    <input type="text" class="form-control" name="login" id="login"
+                           placeholder="Enter login">
+                    <br>
+                    <input type="password" class="password" name="pass" id="pass"
+                           placeholder="Enter pass">
+                    <br>
+                    <button class="btn btn-success" type="submit">Auth</button>
+                    <br>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
-</div>
 <?php else: ?>
-<p>for exit press here <?$_COOKIE['user'] ?> <a href="exit.php"></a></p>
+    <p>for exit press here <?$_COOKIE['user'] ?> <a href="exit.php"></a></p>
 <?php endif;?>
 </body>
 </html>
